342ce4b00f3fc68fa6e6e70df3edfdd96cb5a703
Andrey Trubilo <andreytrubilo@mail.ru>
2023-09-02 23:55:24 +0300

    Security is ready (+ hidden buttons)

--- first_blog/src/main/java/com/andrey/blog/first_blog/controller/FreeBlogController.java
+++ first_blog/src/main/java/com/andrey/blog/first_blog/controller/FreeBlogController.java
@@ -3,6 +3,8 @@ package com.andrey.blog.first_blog.controller;
 import com.andrey.blog.first_blog.models.Post;
 import com.andrey.blog.first_blog.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,14 +23,18 @@ public class FreeBlogController {
     private PostRepository postRepository;
 
     @GetMapping("/blog")
-    public String freeBlog (Model model){
+    public String freeBlog (Model model, @AuthenticationPrincipal User user){
         Iterable<Post> posts = postRepository.findAll();
         model.addAttribute("posts", posts);
+        boolean isAdmin = user != null && user.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+        model.addAttribute("isAdmin", isAdmin);
         return "free-blog";
     }
 
     @GetMapping("/blog/add")
-    public String freeBlogAdd (Model model){
+    public String freeBlogAdd (Model model, @AuthenticationPrincipal User user){
+        boolean isAdmin = user != null && user.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+        model.addAttribute("isAdmin", isAdmin);
         return "blog-add";
     }
 
@@ -36,16 +42,19 @@ public class FreeBlogController {
     public String freeBlogPostAdd(@RequestParam String title,
                                   @RequestParam String anons,
                                   @RequestParam String text,
-                                  Model model, Principal principal){
+                                  Model model, Principal principal,
+                                  @AuthenticationPrincipal User user){
         if(principal==null){return "redirect:/login";}
 
         Post post = new Post(title,anons,text);
         postRepository.save(post);
+        boolean isAdmin = user != null && user.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+        model.addAttribute("isAdmin", isAdmin);
         return "redirect:/blog";
     }
 
     @GetMapping("/blog/{id}")
-    public String freeBlogDetails(@PathVariable(value = "id") long id, Model model){
+    public String freeBlogDetails(@PathVariable(value = "id") long id, Model model, @AuthenticationPrincipal User user){
         if(!postRepository.existsById(id)){
             return "redirect:/blog";
         }
@@ -57,12 +66,15 @@ public class FreeBlogController {
             postRepository.save(p);
             res.add(p);
         });
+
+        boolean isAdmin = user != null && user.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+        model.addAttribute("isAdmin", isAdmin);
         model.addAttribute("post", res);
         return "blog-details";
     }
 
     @GetMapping("/blog/{id}/edit")
-    public String freeBlogEdit (@PathVariable(value = "id") long id, Model model){
+    public String freeBlogEdit (@PathVariable(value = "id") long id, Model model, @AuthenticationPrincipal User user){
         if(!postRepository.existsById(id)){
             return "redirect:/blog";
         }
@@ -71,6 +83,8 @@ public class FreeBlogController {
         ArrayList<Post> res = new ArrayList<>();
         post.ifPresent(res::add);
         model.addAttribute("post", res);
+        boolean isAdmin = user != null && user.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+        model.addAttribute("isAdmin", isAdmin);
         return "blog-edit";
     }
 
@@ -79,7 +93,8 @@ public class FreeBlogController {
                                      @RequestParam String title,
                                      @RequestParam String anons,
                                      @RequestParam String text,
-                                     Model model, Principal principal){
+                                     Model model, Principal principal,
+                                     @AuthenticationPrincipal User user){
         if(principal==null){return "redirect:/login";}
 
         Post post = postRepository.findById(id).orElseThrow();
@@ -87,15 +102,19 @@ public class FreeBlogController {
         post.setAnons(anons);
         post.setText(text);
         postRepository.save(post);
+        boolean isAdmin = user != null && user.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+        model.addAttribute("isAdmin", isAdmin);
 
         return "redirect:/blog";
     }
 
     @PostMapping("/blog/{id}/remove")
     public String freeBlogPostDelete(@PathVariable(value = "id") long id,
-                                     Model model){
+                                     Model model, @AuthenticationPrincipal User user){
         Post post = postRepository.findById(id).orElseThrow();
         postRepository.delete(post);
+        boolean isAdmin = user != null && user.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+        model.addAttribute("isAdmin", isAdmin);
         return "redirect:/blog";
     }
 
--- first_blog/src/main/java/com/andrey/blog/first_blog/controller/MyController.java
+++ first_blog/src/main/java/com/andrey/blog/first_blog/controller/MyController.java
@@ -1,5 +1,7 @@
 package com.andrey.blog.first_blog.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MyController {
 
     @GetMapping("/")
-    public String mainPage (Model model) {
+    public String mainPage (Model model, @AuthenticationPrincipal User user) {
         model.addAttribute("title", "Главная страница");
+
+        boolean isAdmin = user != null && user.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+        model.addAttribute("isAdmin", isAdmin);
         return "main-page";
     }
 
     @GetMapping("/info")
-    public String info (Model model) {
+    public String info (Model model, @AuthenticationPrincipal User user) {
         model.addAttribute("title", "About us");
+        boolean isAdmin = user != null && user.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"));
+        model.addAttribute("isAdmin", isAdmin);
         return "info";
     }
 
