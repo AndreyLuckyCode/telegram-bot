0cf78ea8833a848e78245b553404e2612d63e54f
Andrey Trubilo <andreytrubilo@mail.ru>
2023-09-16 19:01:37 +0300

    + update dialog

--- lucky_job/src/main/java/com/andrey/lucky_job/views/searcher/SearcherViewCard.java
+++ lucky_job/src/main/java/com/andrey/lucky_job/views/searcher/SearcherViewCard.java
@@ -1,11 +1,19 @@
 package com.andrey.lucky_job.views.searcher;
 
+import com.andrey.lucky_job.models.Vacancy;
 import com.andrey.lucky_job.service.VacancyService;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.theme.lumo.LumoUtility.*;
 
 public class SearcherViewCard extends ListItem {
+    private Span header;
+    private Span subtitle;
+    private Paragraph description;
+    private Span badge;
 
     public SearcherViewCard(String company, String requirements, String responsibilities, int salary, Long vacancyId, VacancyService vacancyService) {
         addClassNames(Background.CONTRAST_5, Display.FLEX, FlexDirection.COLUMN, AlignItems.START, Padding.MEDIUM,
@@ -21,30 +29,94 @@ public class SearcherViewCard extends ListItem {
 
         div.add(image);
 
-        Span header = new Span();
+        // Создаем элементы header, subtitle, description, badge
+        header = new Span();
         header.addClassNames(FontSize.XLARGE, FontWeight.SEMIBOLD);
         header.setText(company);
 
-        Span subtitle = new Span();
+        subtitle = new Span();
         subtitle.addClassNames(FontSize.SMALL, TextColor.SECONDARY);
         subtitle.setText(requirements);
 
-        Paragraph description = new Paragraph(responsibilities);
+        description = new Paragraph(responsibilities);
         description.addClassName(Margin.Vertical.MEDIUM);
 
-        Span badge = new Span();
+        badge = new Span();
         badge.getElement().setAttribute("theme", "badge");
         badge.setText(String.valueOf(salary));
 
-        // Создайте кнопку "Delete"
+        Button updateButton = new Button("Update");
+        updateButton.addClickListener(event -> {
+            createEditDialog(company, requirements, responsibilities, salary, vacancyId, vacancyService);
+        });
+
         Button deleteButton = new Button("Delete");
         deleteButton.addClickListener(event -> {
-            // Обработка нажатия на кнопку "Delete"
             vacancyService.deleteVacancy(vacancyId);
-            // Обновите интерфейс, чтобы карточка исчезла
             this.setVisible(false);
         });
 
-        add(div, header, subtitle, description, badge, deleteButton);
+        HorizontalLayout buttonLayout = new HorizontalLayout(updateButton, deleteButton);
+        buttonLayout.setSpacing(true);
+
+        add(div, header, subtitle, description, badge, buttonLayout);
+    }
+
+
+    // Диалоговое окно для изменения карточки
+    private void createEditDialog(String company, String requirements, String responsibilities, int salary, Long vacancyId, VacancyService vacancyService) {
+        Dialog editDialog = new Dialog();
+
+        Vacancy vacancy = vacancyService.getVacancy(vacancyId);
+
+        TextField editedCompany = new TextField("Edited Company");
+        editedCompany.setValue(vacancy.getCompany());
+
+        TextField editedRequirements = new TextField("Edited Requirements");
+        editedRequirements.setValue(vacancy.getRequirements());
+
+        TextField editedResponsibilities = new TextField("Edited Responsibilities");
+        editedResponsibilities.setValue(vacancy.getResponsibilities());
+
+        TextField editedSalary = new TextField("Edited Salary");
+        editedSalary.setValue(String.valueOf(vacancy.getSalary()));
+
+        Button saveButton = new Button("Save");
+        saveButton.addClickListener(event -> {
+            // Достаем изменения из полей
+            String updatedCompany = editedCompany.getValue();
+            String updatedRequirements = editedRequirements.getValue();
+            String updatedResponsibilities = editedResponsibilities.getValue();
+            int updatedSalary = Integer.parseInt(editedSalary.getValue());
+
+            Vacancy updatedVacancy = new Vacancy();
+            updatedVacancy.setId(vacancyId);
+            updatedVacancy.setCompany(updatedCompany);
+            updatedVacancy.setRequirements(updatedRequirements);
+            updatedVacancy.setResponsibilities(updatedResponsibilities);
+            updatedVacancy.setSalary(updatedSalary);
+
+            // Обновляем инфу на беке через сервис
+            vacancyService.updateVacancy(vacancyId, updatedVacancy);
+
+            Vacancy updatedData = vacancyService.getVacancy(vacancyId);
+
+            // Обновляем отображение на фронте
+            updateCardData(updatedData.getCompany(), updatedData.getRequirements(), updatedData.getResponsibilities(), updatedData.getSalary());
+
+            editDialog.close();
+        });
+
+        editDialog.add(editedCompany, editedRequirements, editedResponsibilities, editedSalary, saveButton);
+        editDialog.open();
+    }
+
+
+    // Метод для обновления данных в карточке
+    public void updateCardData(String company, String requirements, String responsibilities, int salary) {
+        header.setText(company);
+        subtitle.setText(requirements);
+        description.setText(responsibilities);
+        badge.setText(String.valueOf(salary));
     }
 }
