28524cab6d20f075e36bcc15ec9506293c0ed120
Andrey Trubilo <andreytrubilo@mail.ru>
2023-09-29 02:32:21 +0300

    + profile fields

--- lucky_job/src/main/java/com/andrey/lucky_job/views/profile/EmployerProfileView.java
+++ lucky_job/src/main/java/com/andrey/lucky_job/views/profile/EmployerProfileView.java
@@ -2,6 +2,7 @@ package com.andrey.lucky_job.views.profile;
 
 import com.andrey.lucky_job.views.MainLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.annotation.VaadinSessionScope;
@@ -16,15 +17,33 @@ import com.andrey.lucky_job.models.Employer;
 @Scope("prototype")
 public class EmployerProfileView extends VerticalLayout {
 
+        private TextField firstNameField = new TextField("First Name");
+        private TextField lastNameField = new TextField("Last Name");
+        private TextField emailField = new TextField("Email");
+        // Добавьте дополнительные поля, которые вы хотите отображать на странице профиля
+
         // Используйте это поле для хранения информации о текущем пользователе
         private Employer currentUser;
 
         public EmployerProfileView() {
-
+                this.add(firstNameField, lastNameField, emailField); // Добавьте дополнительные поля, которые вы хотите отображать на странице профиля
         }
 
         // Установите текущего пользователя с помощью этого метода
         public void setCurrentUser(Employer currentUser) {
-            this.currentUser = currentUser;
+                this.currentUser = currentUser;
+
+                updateProfileFields();
+        }
+
+        private void updateProfileFields() {
+                if (currentUser != null) {
+                        firstNameField.setValue(currentUser.getName());
+                        lastNameField.setValue(currentUser.getSurname());
+                        emailField.setValue(currentUser.getEmail());
+                        // Установите значение для дополнительных полей
+                } else {
+                        // Обработайте отсутствие текущего пользователя, если необходимо
+                }
         }
 }
