b4b757679cba75d5d19257e269ff347ba9ff70ca
Andrey Trubilo <andreytrubilo@mail.ru>
2023-09-29 03:18:40 +0300

    + fields

--- lucky_job/src/main/java/com/andrey/lucky_job/views/profile/EmployerProfileView.java
+++ lucky_job/src/main/java/com/andrey/lucky_job/views/profile/EmployerProfileView.java
@@ -1,6 +1,7 @@
 package com.andrey.lucky_job.views.profile;
 
 import com.andrey.lucky_job.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
@@ -17,19 +18,24 @@ import com.andrey.lucky_job.models.Employer;
 @Scope("prototype")
 public class EmployerProfileView extends VerticalLayout {
 
-        private TextField firstNameField = new TextField("First Name");
-        private TextField lastNameField = new TextField("Last Name");
-        private TextField emailField = new TextField("Email");
-        // Добавьте дополнительные поля, которые вы хотите отображать на странице профиля
+        private final TextField firstNameField = new TextField("First Name");
+        private final TextField lastNameField = new TextField("Last Name");
+        private final TextField emailField = new TextField("Email");
+        private final TextField dateOfBirthField = new TextField("Date of birth");
+        private final TextField phoneNumberField = new TextField("Phone number");
+        private final Button updateProfileInfoButton = new Button("update");
+        private final Button logoutButton = new Button ("Log out");
 
-        // Используйте это поле для хранения информации о текущем пользователе
+
+
+        // Поле для хранения информации о текущем пользователе
         private Employer currentUser;
 
         public EmployerProfileView() {
-                this.add(firstNameField, lastNameField, emailField); // Добавьте дополнительные поля, которые вы хотите отображать на странице профиля
+                this.add(firstNameField, lastNameField, emailField, dateOfBirthField, phoneNumberField, updateProfileInfoButton);
         }
 
-        // Установите текущего пользователя с помощью этого метода
+        // Определить текущего пользователя
         public void setCurrentUser(Employer currentUser) {
                 this.currentUser = currentUser;
 
@@ -41,9 +47,9 @@ public class EmployerProfileView extends VerticalLayout {
                         firstNameField.setValue(currentUser.getName());
                         lastNameField.setValue(currentUser.getSurname());
                         emailField.setValue(currentUser.getEmail());
-                        // Установите значение для дополнительных полей
+                        // значение для дополнительных полей
                 } else {
-                        // Обработайте отсутствие текущего пользователя, если необходимо
+                        // или отсутствие такого пользователя
                 }
         }
 }
