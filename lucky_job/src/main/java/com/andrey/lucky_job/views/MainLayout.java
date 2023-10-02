d65f6a68f658ac752fea4be5c344ca11b6ff7261
Andrey Trubilo <andreytrubilo@mail.ru>
2023-10-02 09:50:24 +0300

    + profile icon and redirect

--- lucky_job/src/main/java/com/andrey/lucky_job/views/MainLayout.java
+++ lucky_job/src/main/java/com/andrey/lucky_job/views/MainLayout.java
@@ -1,5 +1,7 @@
 package com.andrey.lucky_job.views;
 
+import com.andrey.lucky_job.models.Employer;
+import com.andrey.lucky_job.models.Searcher;
 import com.andrey.lucky_job.views.about.AboutView;
 import com.andrey.lucky_job.views.addnewjob.AddNewJobView;
 import com.andrey.lucky_job.views.employer.EmployerView;
@@ -7,25 +9,29 @@ import com.andrey.lucky_job.views.mainpage.MainPageView;
 import com.andrey.lucky_job.views.searcher.SearcherView;
 import com.andrey.lucky_job.views.signin.SigninView;
 import com.andrey.lucky_job.views.signup.SignupView;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.Footer;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.PreserveOnRefresh;
+import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 import org.vaadin.lineawesome.LineAwesomeIcon;
+import com.vaadin.flow.component.icon.Icon;
 
 @PreserveOnRefresh
 public class MainLayout extends AppLayout {
 
     private H2 viewTitle;
-
+    private Icon profileIcon;
     public MainLayout() {
         setPrimarySection(Section.DRAWER);
         addDrawerContent();
@@ -38,8 +44,24 @@ public class MainLayout extends AppLayout {
 
         viewTitle = new H2();
         viewTitle.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
+        // Добавляем иконку профиля.
+        profileIcon = VaadinIcon.USER.create();
+        profileIcon.addClickListener(click -> {
+            // Проверяем, залогинен ли пользователь.
+            Object user = VaadinSession.getCurrent().getAttribute("user");
+            if (user != null) {
+                if (user instanceof Employer) {
+                    UI.getCurrent().navigate("employer-profile");
+                } else if (user instanceof Searcher) {
+                    UI.getCurrent().navigate("searcher-profile");
+                }
+            } else {
+                UI.getCurrent().navigate("login");
+            }
+        });
+        profileIcon.getStyle().set("margin-left", "auto");
 
-        addToNavbar(true, toggle, viewTitle);
+        addToNavbar(true, toggle, viewTitle, profileIcon);
     }
 
     private void addDrawerContent() {
