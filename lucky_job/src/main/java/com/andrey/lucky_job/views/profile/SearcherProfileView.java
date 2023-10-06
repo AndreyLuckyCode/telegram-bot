20dd24851873adeeb1af92d42ea4d991b8a32383
Andrey Trubilo <andreytrubilo@mail.ru>
2023-10-06 07:20:47 +0300

    + profile page (vacancy display)

--- lucky_job/src/main/java/com/andrey/lucky_job/views/profile/SearcherProfileView.java
+++ lucky_job/src/main/java/com/andrey/lucky_job/views/profile/SearcherProfileView.java
@@ -1,16 +1,26 @@
 package com.andrey.lucky_job.views.profile;
 
+import com.andrey.lucky_job.models.CV;
 import com.andrey.lucky_job.models.Employer;
 import com.andrey.lucky_job.models.Searcher;
+import com.andrey.lucky_job.models.Vacancy;
+import com.andrey.lucky_job.service.CVService;
+import com.andrey.lucky_job.service.SearcherService;
+import com.andrey.lucky_job.service.VacancyService;
 import com.andrey.lucky_job.views.MainLayout;
+import com.andrey.lucky_job.views.searcher.SearcherViewCard;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.*;
 import com.vaadin.flow.server.VaadinSession;
 import com.vaadin.flow.spring.annotation.VaadinSessionScope;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @PageTitle("My Profile")
 @Route(value = "searcher-profile", layout = MainLayout.class)
@@ -26,12 +36,18 @@ public class SearcherProfileView extends VerticalLayout implements AfterNavigati
     private final TextField phoneNumberField = new TextField("Phone number");
     private final Button updateProfileInfoButton = new Button("update");
     private final Button logoutButton = new Button("Log out", e -> logout());
-
+    @Autowired
+    private CVService cvService;
+    @Autowired
+    private VacancyService vacancyService;
     private Searcher currentUser;
+    private final VerticalLayout cardsLayout = new VerticalLayout();
+
 
     public SearcherProfileView() {
         this.add(firstNameField, lastNameField, emailField, dateOfBirthField
                 , phoneNumberField, updateProfileInfoButton, logoutButton);
+        this.add(cardsLayout);
     }
 
     @Override
@@ -41,6 +57,7 @@ public class SearcherProfileView extends VerticalLayout implements AfterNavigati
         Searcher currentUser = (Searcher) VaadinSession.getCurrent().getAttribute("user");
         if(currentUser != null) {
             setCurrentUser(currentUser);
+            displayUserVacancies();
         }
     }
 
@@ -74,5 +91,47 @@ public class SearcherProfileView extends VerticalLayout implements AfterNavigati
         System.out.println("Сессия закрыта (searcher)");
 
         getUI().ifPresent(ui -> ui.navigate("login"));
+    }
+
+    private void displayUserVacancies() {
+        cardsLayout.removeAll();
+        String currentUserEmail = currentUser.getEmail();
+
+        List<CV> allCVs = cvService.getAllCVs();
+        List<CV> allCVsOfTheCurrentUser = new ArrayList<>();
+        for (CV cv : allCVs) {
+            if (cv.getAuthor().equals(currentUserEmail)) {
+                allCVsOfTheCurrentUser.add(cv);
+            }
+        }
+        List<Long> vacancyIds = new ArrayList<>();
+
+        for(CV cv : allCVsOfTheCurrentUser){
+                 vacancyIds.add(cv.getVacancyId());
+        }
+
+        // Создаем список для карточек вакансий
+        List<SearcherViewCard> vacancyCards = new ArrayList<>();
+
+        // Создаем и заполняем карточки вакансий для каждого id из списка
+        for (Long vacancyId : vacancyIds) {
+            Vacancy currentVacancy = vacancyService.getVacancy(vacancyId);
+            SearcherViewCard card = new SearcherViewCard(
+                    currentVacancy.getCompany(),
+                    currentVacancy.getRequirements(),
+                    currentVacancy.getResponsibilities(),
+                    currentVacancy.getSalary(),
+                    currentVacancy.getId(),
+                    vacancyService,
+                    true,
+                    cvService,
+                    currentVacancy);
+            vacancyCards.add(card);
+        }
+
+        // Добавляем карточки вакансий на страницу профиля
+        for (SearcherViewCard card : vacancyCards) {
+            cardsLayout.add(card);
+        }
     }
 }
