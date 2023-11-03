6aa9387d4ae96d740ad03d8aefd106354047dc45
Andrey <132314710+AndreyLuckyCode@users.noreply.github.com>
2023-11-03 10:44:39 +0300

    Delete first_blog directory

--- first_blog/src/main/java/com/andrey/blog/first_blog/repository/PostRepository.java
+++
@@ -1,6 +0,0 @@
-package com.andrey.blog.first_blog.repository;
-
-import com.andrey.blog.first_blog.models.Post;
-import org.springframework.data.repository.CrudRepository;
-public interface PostRepository extends CrudRepository<Post, Long> {
-}
