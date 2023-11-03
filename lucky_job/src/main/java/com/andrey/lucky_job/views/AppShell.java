c31636963de33387cb896a9143fa23fc4bb51c0b
Andrey <132314710+AndreyLuckyCode@users.noreply.github.com>
2023-11-03 10:45:37 +0300

    Delete lucky_job directory

--- lucky_job/src/main/java/com/andrey/lucky_job/views/AppShell.java
+++
@@ -1,9 +0,0 @@
-package com.andrey.lucky_job.views;
-
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.lumo.Lumo;
-
-@Theme(value = "luckyjob", variant = Lumo.DARK)
-public class AppShell implements AppShellConfigurator {
-}
